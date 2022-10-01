959a1de66f26137b31a18ed3fc70d8e39c32c893
KeyOu <9keyoujob9@gmail.com>
2022-10-01 17:07:09 +0300

    Fourth task

--- Program.cs
+++ Program.cs
@@ -55,3 +55,15 @@ if (number % 2 == 0)
 else
 
     Console.WriteLine($"Число {number} нечетное");
+
+/*Задача 4: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i <= num; i += 2)
+
+    Console.WriteLine($"{i}");
